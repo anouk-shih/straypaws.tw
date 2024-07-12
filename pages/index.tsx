e5d503b34a3a7574fbc6c4464b7d3993ab3bbf17
Anouk Shih <47846927+anouk-shih@users.noreply.github.com>
2024-07-12 18:30:27 +0800

    add new entry

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+import Dashboard from "@/components/Dashboard";
+
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <Dashboard />;
+};
+
+export default Home;
