1fd47d2fb3a745dd4fa35186654c3582e1b56400
Anouk Shih <47846927+anouk-shih@users.noreply.github.com>
2024-07-12 18:31:34 +0800

    add dashboard

---
+++ components/Dashboard.tsx
@@ -0,0 +1,28 @@
+// components/Dashboard.tsx
+import React from "react";
+
+// import StrayChart from "./StrayChart";
+// import StrayMap from "./StrayMap";
+import { useShelterSummary } from "@/hooks/useOpenAPI";
+
+const Dashboard: React.FC = () => {
+  const { data, error } = useShelterSummary();
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>Stray Animal Dashboard</h1>
+      <ul>
+        {data.map((shelter) => (
+          <li key={shelter.ID}>
+            {shelter.rpt_country}: {shelter.adopt_num} adoptions
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Dashboard;
